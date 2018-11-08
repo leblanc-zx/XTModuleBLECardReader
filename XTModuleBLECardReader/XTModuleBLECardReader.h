fcc1561c00ef2fcb339b03fd5ae5a1717c6eaacc
351706354@qq.com <351706354@qq.com>
2018-11-08 16:32:30 +0800

    Initial Commit

---
+++ XTModuleBLECardReader/XTModuleBLECardReader.h
@@ -0,0 +1,19 @@
+//
+//  XTModuleBLECardReader.h
+//  XTModuleBLECardReader
+//
+//  Created by apple on 2018/11/8.
+//  Copyright © 2018年 新天科技股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XTModuleBLECardReader.
+FOUNDATION_EXPORT double XTModuleBLECardReaderVersionNumber;
+
+//! Project version string for XTModuleBLECardReader.
+FOUNDATION_EXPORT const unsigned char XTModuleBLECardReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XTModuleBLECardReader/PublicHeader.h>
+
+
